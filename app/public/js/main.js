627a3c4951a86500be2d387805216a5a2e922356
Tak Tran <contact@tutaktran.com>
2013-09-27 21:48:22 +0100

    Use backbone models for player data.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -5,6 +5,29 @@
   var myPath;
   var animation;  //make the variables global, so you can access them in the animation function
 
+  var Player = Backbone.Model.extend({
+    initialize: function(num) {
+      this.num = num;
+
+      // Only do ship 1 for now
+      this.elem = $(".p" + num + "-ship-1");
+    },
+    // Half way
+    x: function() {
+      var left = this.elem.offset()['left'],
+        width = this.elem.width(),
+        x = left + (width / 2);
+
+      return x;
+    },
+    y: function() {
+      var top = this.elem.offset()['top'],
+        height = this.elem.height();
+
+      // Include height for player 1
+      return (this.num === 1) ? top + height : top;
+    }
+  });
 
   $(function() {
     var canvas = {
@@ -47,37 +70,8 @@
     }
 
     function init() {
-      var player1 = {
-          // Half way
-          x: function() {
-            var left = $(".p1-ship-1").offset()['left'],
-              width = $(".p1-ship-1").width(),
-              x = left + (width / 2);
-
-            return x;
-          },
-          y: function() {
-            var top = $(".p1-ship-1").offset()['top'],
-              height = $(".p1-ship-1").height();
-
-            return top + height;
-          }
-        },
-        player2 = {
-          // Half way
-          x: function() {
-            var left = $(".p2-ship-1").offset()['left'],
-              width = $(".p2-ship-1").width(),
-              x = left + (width / 2);
-
-            return x;
-          },
-          y: function() {
-            var top = $(".p2-ship-1").offset()['top'];
-
-            return top;
-          }
-        };
+      var player1 = new Player(1),
+        player2 = new Player(2);
 
       $(".p1-ship").click(function() {
         curve(player1.x(), player1.y(), player2.x(), player2.y(), "blue");
