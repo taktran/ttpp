22c0a7cea5b719212ddd21cd6821f7d8b0ca1bf5
Tak Tran <contact@tutaktran.com>
2013-09-27 22:05:10 +0100

    Hook up line and click.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,6 +1,8 @@
 (function (){
   'use strict';
 
+  var ANIMATION_INTERVAL = 5; // milliseconds
+
   var e;
   var myPath;
   var animation;  //make the variables global, so you can access them in the animation function
@@ -29,53 +31,67 @@
     }
   });
 
-  $(function() {
-    var canvas = {
-      height: window.outerHeight,
-      width: window.outerWidth
-    };
+  // Pew pew pew! Fire pew view
+  var Pew = Backbone.View.extend({
+    initialize: function(canvas, attackPlayerModel, receiverPlayerModel, shipNum, color) {
+      this.canvas = canvas;
+      this.attackPlayer = attackPlayerModel;
+      this.receiverPlayer = receiverPlayerModel;
+      this.shipNum = shipNum;
+      this.color = color;
 
-    var r = new Raphael("canvas", canvas.width, canvas.height),
-      ANIMATION_INTERVAL = 5; // milliseconds
+      this.counter = 0;    // a counter that counts animation steps
+    },
 
-
-    var counter = 0;    // a counter that counts animation steps
-    var animate = function() {
-      if (myPath.getTotalLength() <= counter){   //break as soon as the total length is reached
+    animate: function() {
+      if (myPath.getTotalLength() <= this.counter){   //break as soon as the total length is reached
         clearInterval(animation);
         return;
       }
-      var pos = myPath.getPointAtLength(counter);   //get the position (see Raphael docs)
+      var pos = myPath.getPointAtLength(this.counter);   //get the position (see Raphael docs)
       e.attr({cx: pos.x, cy: pos.y});  //set the circle position
 
-      counter++; // count the step counter one up
-    };
+      this.counter++; // count the step counter one up
+    },
 
-    function curve(initialX, initialY, finalX, finalY, colour) {
+    curve: function(initialX, initialY, finalX, finalY, colour) {
       var ax = Math.floor(Math.random() * 200) + initialX;
       var ay = Math.floor(Math.random() * 200) + (initialY - 100);
       var bx = Math.floor(Math.random() * 200) + (finalX - 200);
       var by = Math.floor(Math.random() * 200) + (finalY - 100);
-      e = r.circle(initialX, initialY, 5, 5).attr({
+      e = this.canvas.circle(initialX, initialY, 5, 5).attr({
         stroke: "none",
         fill: colour
       });
       var path = [["M", initialX, initialY], ["C", ax, ay, bx, by, finalX, finalY]];
-      myPath = r.path(path).attr({
+      myPath = this.canvas.path(path).attr({
         stroke: colour,
         "stroke-width": 2,
         "stroke-linecap": "round",
         "stroke-opacity": 1
       });
+    },
+
+    // Fire pew pew
+    render: function() {
+      this.curve(this.attackPlayer.x(), this.attackPlayer.y(), this.receiverPlayer.x(), this.receiverPlayer.y(), this.color);
+      animation = setInterval(this.animate, ANIMATION_INTERVAL);
     }
+
+  });
+
+  $(function() {
+    var height = window.outerHeight,
+      width = window.outerWidth,
+      canvas = new Raphael("canvas", width, height);
 
     function init() {
       var player1 = new Player(1),
         player2 = new Player(2);
 
       $(".p1-ship").click(function() {
-        curve(player1.x(), player1.y(), player2.x(), player2.y(), "blue");
-        animation = setInterval(animate, ANIMATION_INTERVAL);
+        var pew = new Pew(canvas, player1, player2, 1, "blue");
+        pew.render();
       });
     }
 
