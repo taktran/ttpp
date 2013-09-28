f9934d030ccbc45641d386b6a505b4b043f2fda1
Tak Tran <contact@tutaktran.com>
2013-09-28 12:00:33 +0100

    Got lights working with arduino.

--- bin/server.js
+++ bin/server.js
@@ -1,15 +1,52 @@
-"use strict";
+'use strict';
 
-var nodeStatic = require('node-static');
+var Primus = require('primus'),
+    http = require('http'),
+    five = require("johnny-five");
 
-var file = new(nodeStatic.Server)('./app/public'),
-  port = process.env.PORT || 8080;
+// ------------------------------------------------
+// Johnny five
+// ------------------------------------------------
 
-require('http').createServer(function(request, response) {
-  file.serve(request, response, function(err, res) {
-    if (err) {
-      response.writeHead(err.status, err.headers);
-      response.end();
-    }
+five.Board({
+  port: "/dev/cu.usbmodem621" // tmac top usb
+}).on("ready", function() {
+  var rgb1, rgb2;
+
+  rgb1 = new five.Led.RGB([3, 5, 6]);
+  rgb2 = new five.Led.RGB([9, 10, 11]);
+  // rainbow = [ "FF000", "FF7F00", "00FF00", "FFFF00", "0000FF", "4B0082", "8F00FF" ];
+  // rainbow = [ [0, 0, 0], [255, 0, 0], [0, 255, 0], [0, 0, 255], [255, 255, 255] ];
+
+  this.repl.inject({
+    rgb1: rgb1,
+    rgb2: rgb2
   });
-}).listen(port);
+
+  rgb1.color([50,0,0]); // Red
+  rgb2.color([0,50,0]); // Green
+});
+
+// ------------------------------------------------
+// Server
+// ------------------------------------------------
+
+// var server = http.createServer(),
+//   primus = new Primus(server, {
+//     transformer: 'sockjs'
+//   });
+
+// primus.on('connection', function(spark) {
+//   console.log('connection:\t', spark.id);
+
+//   spark.on('data', function(data) {
+//     spark.write("Server says '" + data + "'");
+//   });
+// });
+
+// primus.on('disconnection', function(spark) {
+//   console.log('disconnection:\t', spark.id);
+// });
+
+// console.log(' [*] Listening on 0.0.0.0:9999' );
+// server.listen(9999, '0.0.0.0');
